108fd851b25bd74d4a23db91e0ea340b5ed28211
fangqijun <zfangqj@gmail.com>
2024-11-28 23:03:52 +0800

    用户协议和隐私政策

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import everfnLogo from "./statics/everfn.webp";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <Image src={everfnLogo} alt="Everfn Logo" width={48} height={48} />,
+  head: (
+    <>
+      <link rel="icon" href={everfnLogo.src} />
+      <title>Block X 在线手册</title>
+    </>
+  ),
   footer: {
-    text: 'Nextra Docs Template',
+    text: "© 2024-2025 永动方程",
   },
-}
+  editLink: {
+    component: ({ filePath }) => null,
+  },
+  feedback: {
+    content: null,
+  },
+};
 
-export default config
+export default config;

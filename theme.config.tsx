884d1e4fc5138d66f6117b233d68fa2d62ec5787
fangqijun <zfangqj@gmail.com>
2024-11-28 23:56:57 +0800

    配置head和logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import everfnLogo from "./statics/everfn.webp";
 import Image from "next/image";
+import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <Image src={everfnLogo} alt="Everfn Logo" width={48} height={48} />,
-  head: (
-    <>
-      <link rel="icon" href={everfnLogo.src} />
-      <title>Block X 在线手册</title>
-    </>
+  logo: (
+    <span
+      style={{
+        fontFamily: "monospace",
+        fontWeight: "bold",
+      }}
+    >
+      Block X 在线手册
+    </span>
   ),
+  head: () => {
+    const { title } = useConfig();
+    return (
+      <>
+        <link rel="icon" href={everfnLogo.src} />
+        <title>{title}</title>
+      </>
+    );
+  },
   footer: {
     text: "© 2024-2025 永动方程",
   },
